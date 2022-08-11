b48c20c8b045122c909510842e5e937cf08fe977
oluwajuwonloblaze <109973264+oluwajuwonloblaze@users.noreply.github.com>
2022-08-11 15:58:59 +0100

    Update signin.php

--- my blog/signin.php
+++ my blog/signin.php
@@ -1,22 +1,7 @@
 <?php
 $username ='';
 $password = '';
-if (isset($_POST['submit'])) {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-$a = new db;
-$sqlquery = "SELECT * FROM users_table  WHERE username = '$username'";
-$read = mysqli_query($con, $sqlquery);
-if ((mysqli_num_rows($read) > 0)) {
-echo 'exists';
-}
-else {
-  echo 'errors';
-}
-}
 ?>
-
 <form action="signin.php">
     <label for="username">username</label>
     <input type="text"  name = "username"  value = "<?php  echo $username  ?>">
